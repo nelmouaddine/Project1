493ebc9563c5e86cba5e89178478f9699d390f3f
nelmouaddine <najatelmouaddine@gmail.com>
2020-01-08 15:44:02 +0000

    Commit1

--- student/add.php
+++ student/add.php
@@ -1,16 +1,17 @@
 <?php
-$nameError = $mobileError = "";
+$nameError = $emailError = $mobileError ="";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = $_POST['name'];
     $mobile = $_POST['mobile'];
+    $email = $_POST['email'];
     if(!preg_match("/^[a-zA-Z ]+$/", $name)) {
         $nameError = "Le nom saisi est incorrect.";
     }
     if(!preg_match("/^[0-9]{10}$/", $mobile)) {
         $mobileError = "Le format du numéro de tél. est incorrect.";
     }
-    if(!preg_match("/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+$/", $emailError)) {
+    if(!preg_match("/^[a-zA-Z0-9\.\-_]+@[a-zA-Z0-9]+\.[a-zA-Z]+$/", $email)) {
         $emailError = "Le format de l'email est incorrect.";
     }
 }
@@ -27,15 +28,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         <label for="cne">CNE</label>
         <input type="text" name="cne"/><br>
         <label for="name">Nom</label>
-        <span class="error"><?php echo $nameError; ?></span><br>
         <input type="text" name="name"/><br>
+        <span class="error"><?php echo $nameError; ?></span><br>
         <label for="birth_date">Date de naissance</label>
         <input type="text" name="birth_date"/><br>
         <label for="address">Adresse</label>
         <input type="text" name="address"/><br>
         <label for="email">Email</label>
-        <span class="error"><?php echo $emailError; ?></span><br>
         <input type="text" name="email"/><br>
+        <span class="error"><?php echo $emailError; ?></span><br>
         <label for="mobile">Tél</label>
         <input type="text" name="mobile"/>
         <span class="error"><?php echo $mobileError; ?></span><br>

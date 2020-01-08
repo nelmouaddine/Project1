041c2f8200f78c42dec76fd0b760e0d99d1e9157
nelmouaddine <najatelmouaddine@gmail.com>
2020-01-08 14:50:48 +0000

    Commit1

---
+++ user/login.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Authentification</title>
+</head>
+<body>
+<form action="index.php" method="get">
+    <label for="email">Email</label>
+    <input type="email" name="email"><br>
+    <label for="password">Mot de passe</label>
+    <input type="password" name="password"><br>
+    <input type="submit" value="Se connecter">
+</form>
+
+</body>
+</html>

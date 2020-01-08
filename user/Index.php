9a88b2d14f2460de5383570ac2dd1f0b8a16ba2c
nelmouaddine <najatelmouaddine@gmail.com>
2020-01-08 15:00:53 +0000

    Commit1

---
+++ user/Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$email = $_GET['email'];
+$password = $_GET['password'];
+if($email == 'a@a.com' && $password == 'admin') {
+    echo $_GET['email']."<br>";
+}
+?>
+</body>
+</html>

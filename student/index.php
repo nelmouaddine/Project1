b22e67896f212e51de82c1d40720b4a98ecac319
nelmouaddine <najatelmouaddine@gmail.com>
2020-01-08 14:48:19 +0000

    Commit1

---
+++ student/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<form action="index.php" method="get">
+    <label for="CNE">CNE</label>
+    <input type="CNE" name="CNE"><br>
+    <label for="nom">Nom</label>
+    <input type="nom" name="nom"><br>
+    <label for="prenom">Prénom</label>
+    <input type="prenom" name="prénom"><br>
+    <label for="Adresse">Adresse</label>
+    <input type="adresse" name="adresse"><br>
+    <input type="submit" value="Envoyer">
+
+</form>
+
+</body>
+</html>
+
